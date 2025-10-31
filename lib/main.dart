3f9a0abf7b180e7671e1ac8617161e0c7121fdb6
vanstrong4 <alfeushendri@gmail.com>
2025-11-01 00:07:27 +0700

    finish

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../screen/splash_screen_1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "NIM : 1123150186, evan",
+      theme: ThemeData(useMaterial3: true,colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),),
+      debugShowCheckedModeBanner: false,
+      home: MySplashScreen(),
+    );
+  }
+}
